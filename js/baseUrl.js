d8b9139e3ec9cdd34cb55a5073efa6ea78304d69
yhlben <yinhengliben@gmail.com>
2018-10-28 20:38:43 +0800

    绑定java模板

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,28 +1,14 @@
-var BASE_URL = 'http://106.12.145.154:2989'
+var BASE_URL = 'http://oauth.ycyc580.com';
 
-function importScript(src,callback){
-    var script =  document.createElement('script');
-    script.src = src;
+function importScript(src, callback) {
+  var script = document.createElement('script');
+  script.src = src;
 
-    document.head.appendChild(script);
+  document.head.appendChild(script);
 
-    return new Promise((resolve,reject) => {
-        script.onload = function(){
-            resolve();
-        }
-    });
+  return new Promise((resolve, reject) => {
+    script.onload = function() {
+      resolve();
+    };
+  });
 }
-
-// function ready(){
-
-// }
-
-
-// window.addEventListener('load',function(){
-//     importScript('./js/jquery-3.3.1.min.js').then(()=>{
-//         $.ajaxSetup({
-//             url: BASE_URL,
-//         });
-//         ready();
-//     });
-// });
